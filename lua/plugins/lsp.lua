75a7b251d5845b0afa21c41b2fe8287d1f8b518a
Michael2061 <michael.roehm@outlook.com>
2026-01-11 11:40:08 +0100

    change code

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,59 +1,86 @@
 return {
-  { 
-    "williamboman/mason.nvim", 
-    opts = { ui = { border = "rounded" } } -- Hier die "rounded" Optik hinzugefügt
+  {
+    "williamboman/mason.nvim",
+    opts = { ui = { border = "rounded" } }
   },
-  { 
-    "williamboman/mason-lspconfig.nvim", 
+  {
+    "williamboman/mason-lspconfig.nvim",
     opts = {
-      -- Hier die erweiterte Liste für fast alle Sprachen
-      ensure_installed = { 
-        "lua_ls", "pyright", "ts_ls", "html", "cssls", "jsonls", 
+      ensure_installed = {
+        "lua_ls", "pyright", "ts_ls", "html", "cssls", "jsonls",
         "bashls", "rust_analyzer", "clangd", "gopls", "yamlls",
         "marksman", "dockerls", "sqlls"
       },
-      automatic_installation = true, -- Installiert Server automatisch beim Öffnen
+      automatic_installation = true,
     }
   },
   {
     "neovim/nvim-lspconfig",
     dependencies = { 'saghen/blink.cmp' },
     config = function()
-      -- Hier müssen die neuen Server auch in die Liste, damit sie aktiviert werden
-      local servers = { 
-        "lua_ls", "pyright", "ts_ls", "html", "cssls", "jsonls", 
-        "bashls", "rust_analyzer", "clangd", "gopls", "yamlls",
-        "marksman", "dockerls", "sqlls" 
-      }
-      
-      local capabilities = require('blink.cmp').get_lsp_capabilities()
+    local servers = {
+      "lua_ls", "pyright", "ts_ls", "html", "cssls", "jsonls",
+      "bashls", "rust_analyzer", "clangd", "gopls", "yamlls",
+      "marksman", "dockerls", "sqlls"
+    }
 
-      for _, server in ipairs(servers) do
+    local capabilities = require('blink.cmp').get_lsp_capabilities()
+
+    for _, server in ipairs(servers) do
+      -- Hier erstellen wir die Optionen für den Server
+      local server_opts = {
+        capabilities = capabilities,
+      }
+
+      -- SPEZIELLER FIX FÜR CSS (Waybar @color Variablen)
+      if server == "cssls" then
+        server_opts.settings = {
+          css = { lint = { unknownAtRules = "ignore" } }
+        }
+        end
+
         -- Native 0.11 API Konfiguration
         vim.lsp.config(server, {
           install = {
             cmd = function(dispatchers)
-              return require('lspconfig.configs.' .. server).default_config.cmd
+            return require('lspconfig.configs.' .. server).default_config.cmd
             end
           },
-          options = {
-            capabilities = capabilities,
-          }
+          options = server_opts, -- Hier werden unsere Einstellungen (inkl. CSS Fix) übergeben
         })
-        -- Aktiviert den Server
-        vim.lsp.enable(server)
-      end
 
-      -- Keymaps
-      vim.api.nvim_create_autocmd('LspAttach', {
-        callback = function(ev)
+        vim.lsp.enable(server)
+        end
+
+        -- 1. Keymaps & Diagnostics Fix
+        vim.api.nvim_create_autocmd('LspAttach', {
+          callback = function(ev)
           local opts = { buffer = ev.buf }
           vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
           vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
-          -- Tipp: Füge noch 'gr' für Referenzen hinzu
           vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
-        end,
-      })
-    end
+          -- Manueller Shortcut für Fehler-Fenster
+          vim.keymap.set('n', 'gl', vim.diagnostic.open_float, opts)
+          end,
+        })
+
+        -- 2. AUTOMATISCHES FEHLER-FENSTER (CursorHold)
+        vim.api.nvim_create_autocmd("CursorHold", {
+          callback = function()
+          local opts = {
+            focusable = false,
+            close_events = { "BufLeave", "CursorMoved", "InsertEnter", "FocusLost" },
+            border = 'rounded',
+            source = 'always',
+            prefix = ' ',
+            scope = 'cursor',
+          }
+          vim.diagnostic.open_float(nil, opts)
+          end
+        })
+
+        -- 3. Geschwindigkeit der Anzeige anpassen (300ms statt 4s)
+        vim.opt.updatetime = 300
+        end
   }
 }
--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,16 +1,14 @@
 return {
   {
     'nvim-telescope/telescope.nvim',
-    tag = '0.1.8',
     dependencies = { 'nvim-lua/plenary.nvim' },
     config = function()
-      local builtin = require('telescope.builtin')
-      -- Dateisuche mit Leertaste + ff
-      vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
-      -- Textsuche in allen Dateien mit Leertaste + fg
-      vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
-      -- Suche in offenen Tabs mit Leertaste + fb
-      vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
+    local builtin = require('telescope.builtin')
+    require('telescope').setup({})
+
+    vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = "Find Files" })
+    vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = "Live Grep" })
+    vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = "Buffers" })
     end
   }
 }
